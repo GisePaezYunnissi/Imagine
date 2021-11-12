8c3e4c2a7a7043f41a33a6b717f8c1619aa7cb3a
gisellapaezyunnissi <gisesabri@hotmail.com>
2021-11-12 12:37:04 -0300

    Correción de errores en js

--- js/ecommerce.js
+++ js/ecommerce.js
@@ -1,108 +1,96 @@
-// Constructor de objeto a modelar los combos
 class Ecommerce {
-
     constructor() {
-    this.combos = [];
-    this.carrito = [];
-    }
-    // Cargo los combos disponibles
-    armarCombos(){
+        this.combos = [];
+        this.carrito = [];
+};
+// Cargo los combos disponibles
+armarCombos(){
     //Llamo AJAX para cargar los combos
-        $.get('combos.json',function(response, state){
+    $.get('combos.json',function(response, state){
         this.combos= response;
         localStorage.ajax = JSON.stringify(response); 
-        });
-    }
-    cargarCombos(){
-        this.combos = JSON.parse(localStorage.ajax);
-    }
-        
-    //Se muestra en html las tarjetas de los distintos combos con el nombre y precio
-    mostrarCards(){
-        this.armarCombos();
-        let acumular =``;
-        this.combos.forEach(combo => {
-            acumular += ` 
-            <article class="card-carrito">
+        miCarrito.cargarCombos();
+        miCarrito.mostrarCards();
+    });
+};
+cargarCombos(){
+    this.combos = JSON.parse(localStorage.ajax);
+};     
+//Se muestra en html las tarjetas de los distintos combos
+mostrarCards(){
+    let acumular =``;
+    this.combos.forEach(combo => {
+        acumular += ` 
+        <article class="card-carrito">
             <div class="peliculas_container">
-            <div>
-            <img style="width:290px" src='${combo.src}' alt='${combo.alt}'>
-            </div>
+                <div>
+                    <img style="width:290px" src='${combo.src}' alt='${combo.alt}'>
+                </div>
             </div>
             <div class="carrito_container">
-            <h1>${combo.nombre}</h1>
-            <h2>$${combo.price}</h2>
-            <button data-idcombo = "${combo.id}" class="btn-agregarAlCarrito">Agregar</button>
+                <h1>${combo.nombre}</h1>
+                <h2>$${combo.price}</h2>
+                <button data-idcombo = "${combo.id}" class="btn-agregarAlCarrito">Agregar</button>
             </div>
-            </article>`
-        });
+        </article>`
+    });
         
-        document.getElementById ("combos").innerHTML = acumular;
-        /*$("#combos").html(acumular);*/
-
-        this.cargarBotones()
-    }
-    //Se identifica que boton hace clik para poder cargar en el carrito
-    cargarBotones() {
-        const arrayDeBotones = Array.from(document.getElementsByClassName('btn-agregarAlCarrito'));
-        arrayDeBotones.forEach(boton => { 
-        boton.onclick = (event) => {
-        const responsableID = event.target.getAttribute("data-idcombo");
-        /* console.log(responsableID); */
-        this.agregarAlCarrito(responsableID)
-            }
-        })
+    document.getElementById ("combos").innerHTML = acumular;
+    this.cargarBotones();
+};
+//Se identifica que boton hace clik para poder cargar en el carrito
+cargarBotones() {
+    const arrayDeBotones = Array.from(document.getElementsByClassName('btn-agregarAlCarrito'));
+    arrayDeBotones.forEach(boton => { boton.onclick = (event) => {
+    const responsableID = event.target.getAttribute("data-idcombo");
+    this.agregarAlCarrito(responsableID)}});
         
-        $('#vaciar').html('<button id="btnJQuery">Vaciar carrito</button>')
-        $("#btnJQuery").on('click', () => {this.vaciarCarrito()});
-        $('#comprar').html('<button id="btnJQuery2">Comprar</button>')
-        $("#btnJQuery2").on('click',()=>(this.realizarCompra()))
-    }
+    $('#vaciar').html('<button id="btnJQuery">Limpiar</button>')
+    $("#btnJQuery").on('click', () => {this.vaciarCarrito()});
+    $('#comprar').html('<button id="btnJQuery2">Comprar</button>')
+    $("#btnJQuery2").on('click',()=>(this.realizarCompra()))
+};
 
-    agregarAlCarrito(id) {
-        let existente=this.carrito.findIndex(element =>element.id==id)
-        if (existente==-1) {
-        const comboAgregar = this.combos.find(combo => {
+agregarAlCarrito(id) {
+    let existente=this.carrito.findIndex(element =>element.id==id)
+    if (existente==-1) {const comboAgregar = this.combos.find(combo => {
         return combo.id == id;
     });
     this.carrito.push(comboAgregar);
     swal("Perfecto", "Agregaste un combo al carrito", "success");
     }
-    else{
+    else {
     swal("Lo siento", "No se puede seleccionar el mismo combo más de una vez", "error");
     } 
-    //Total de mi carrito
+//Total de mi carrito
     let total = 0;
-    this.carrito.forEach(combo => {
-    total += combo.price
-    //Muestro el total en pesos de los combos en el carrito
+    this.carrito.forEach(combo => {total += combo.price
+//Muestro el total en pesos de los combos en el carrito
     document.getElementById("total").innerHTML = "$"+total;
-    // Muestro la cantidad de combos agregados al carrito
+// Muestro la cantidad de combos agregados al carrito
     document.getElementById("cantidad").innerHTML = `Cantidad: ${this.carrito.length}`
     });
-    
-    //localStorage
+//localStorage
     localStorage.total = JSON.stringify(total);
     localStorage.cantidad = JSON.stringify(this.carrito.length); 
     localStorage.combo = JSON.stringify(this.carrito);
-}
+};
 //Borrar carrito en localstorage y html
-    vaciarCarrito(){
-    localStorage.total =0
-    localStorage.cantidad=0
-    localStorage.combo=[]
-    this.total=0
-    this.cantidad=0
-    this.carrito = []
+vaciarCarrito(){
+    localStorage.total =0;
+    localStorage.cantidad=0;
+    localStorage.combo=[];
+    this.total=0;
+    this.cantidad=0;
+    this.carrito = [];
     this.mostrarCarrito = [];
     document.getElementById("total").innerHTML = "$0";
-    document.getElementById("cantidad").innerHTML = `Cantidad: 0`
-}
-
+    document.getElementById("cantidad").innerHTML = "Cantidad: 0";
+};
 realizarCompra(){
     swal({
         title: "Compra realizada con exito",
-        text: "Dentro de los 10 días lo vas a estar recibiendo ¡Muchas gracias!",
+        text: "Dentro de los próximos 10 días lo vas a estar recibiendo ¡Muchas gracias!",
         icon: "success",
       });
     localStorage.total =0;
@@ -113,7 +101,6 @@ realizarCompra(){
     this.carrito=[];
     this.mostrarCarrito=[];
     document.getElementById("total").innerHTML = "$0";
-    document.getElementById("cantidad").innerHTML = `Cantidad: 0`
-  }
-
+    document.getElementById("cantidad").innerHTML = "Cantidad: 0";
+};
 }
--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
 const miCarrito = new Ecommerce();
-
 miCarrito.armarCombos();
-miCarrito.cargarCombos();
-miCarrito.mostrarCards();
-
 // Corroboro si hay algo en el carrito
 if(localStorage.miCarrito) {
     miCarrito.carrito = JSON.parse(localStorage.combo);
